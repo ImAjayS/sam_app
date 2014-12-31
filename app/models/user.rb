86672c409e5d60faac4ace80719a0ff78e9adea6
Ajay <ajaykumar.s@live.com>
2014-12-31 13:57:13 +0530

    chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
    validates :name, presence: true, length: {maximum: 50 }
    validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }, uniqueness: {case_sensitive: false }
    has_secure_password
-   validates :password, length: { minimum: 6 }
+   validates :password, length: { minimum: 6 }, allow_blank: true
 
    def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
